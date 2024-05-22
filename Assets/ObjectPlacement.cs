8af65690ac24bfea7d2907d69ea4699ad85a37f5
OEO <oogretmen19@ku.edu.tr>
2024-05-22 19:40:33 +0300

    Initial commit

---
+++ Assets/ObjectPlacement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectPlacement : MonoBehaviour
+{
+
+    public GameObject targetobj;
+    public TMP_Text TextArea;
+    public string SuccessNote;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (targetobj == null || TextArea == null)
+            return;
+        else
+            if(other.gameObject ==  targetobj)
+        {
+            Debug.Log("Object is Placed");
+            TextArea.text = "Tebrikler; "+ SuccessNote +" Yerinde.";
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (targetobj == null || TextArea == null)
+            return;
+        else
+            if (other.gameObject == targetobj)
+        {
+            Debug.Log("Object is removed");
+            TextArea.text = SuccessNote + " Yerinde Değil!";
+        }
+    }
+}
